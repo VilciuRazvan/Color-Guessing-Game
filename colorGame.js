656910c47cd3035cd142bae3315a7597a698f250
Razvan <35446534+soperb@users.noreply.github.com>
2020-07-07 16:07:12 +0300

    design changes

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,44 @@
-let colors = generateRandomColors(6);
-
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.querySelector("#colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let easyBTN = document.querySelector("#easyBTN");
+let hardBTN = document.querySelector("#hardBTN");
+
+
+easyBTN.addEventListener("click", function () {
+    easyBTN.classList.add("selected");
+    hardBTN.classList.remove("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.display = "none";
+        }
+    }
+})
+
+hardBTN.addEventListener("click", function () {
+    hardBTN.classList.add("selected");
+    easyBTN.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,6 +52,8 @@ for (let i = 0; i < squares.length; i++) {
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?";
+
         }
         else {
             this.style.backgroundColor = "#232323";
@@ -37,19 +73,31 @@ function pickColor() {
     return colors[random];
 }
 
-function generateRandomColors(number){
-    let arr =[];
+function generateRandomColors(number) {
+    let arr = [];
 
-    for (let i = 0 ; i < number ; i++){
+    for (let i = 0; i < number; i++) {
         arr.push(randomColor());
     }
 
     return arr;
 }
 
-function randomColor(){
+function randomColor() {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
-    return "rgb(" + red + ", " + green + ", " + blue +")";
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
+
+resetButton.addEventListener("click", function () {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+    resetButton.textContent = "New Colors";
+})
